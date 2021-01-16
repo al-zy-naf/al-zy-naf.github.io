1d5d11e8db533c240181b257334bf1ef4ee9f6ef
al-zy-naf <alzynaf123@gmail.com>
2021-01-17 00:12:52 +0700

    Added onload and onunload at js_index.js

--- js_index.js
+++ js_index.js
@@ -1,3 +1,12 @@
+
+window.onload = function () {
+    console.log(i);
+}
+var i=0
 function notifyMe() {
     document.getElementById("ptag").innerHTML += " SUCCESS"
+}
+
+window.onunload=function(){
+    i+=1
 }
