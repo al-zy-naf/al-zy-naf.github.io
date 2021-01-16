f9fd3c44f116a7c48902badbd08f3f1986e7a0d0
al-zy-naf <alzynaf123@gmail.com>
2021-01-16 23:59:38 +0700

    Added ptag

--- js_index.js
+++ js_index.js
@@ -1,20 +1,3 @@
-function notifyMe(params) {
-    if (!("Notification" in window)) {
-        alert("This Browser doesn't support Notifications")
-    }
-
-    else if (Notification.permission=="granted") {
-        var notification = new Notification("Hi There!") 
-    }
-
-    else if(Notification.permission=="default"){
-        Notification.requestPermission().then(function (permission) {
-            if (permission=="granted") {
-                var notification = new Notification("User Granted The Permission")
-            }else if (permission=="denied") {
-                alert("User denied the permission")
-            }
-        })
-    }
-    
+function notifyMe() {
+    document.getElementById("ptag").innerHTML += " SUCCESS"
 }
